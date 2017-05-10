63fa5617de12bff97253f6b69b8d3b576e1f13f9
Michał Pierzchała <thymikee@gmail.com>
2017-05-10 10:26:55 +0200

    Setup jest

--- ng-custom/test/setupJest.ts
+++ ng-custom/test/setupJest.ts
@@ -1,24 +1,2 @@
-import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
-import 'zone.js';
-import 'zone.js/dist/proxy.js';
-import 'zone.js/dist/sync-test';
-import 'zone.js/dist/async-test';
-import 'zone.js/dist/fake-async-test';
-import 'jest-zone-patch';
-import { AngularSnapshotSerializer } from './AngularSnapshotSerializer';
-import { HTMLCommentSerializer } from './HTMLCommentSerializer';
-import { getTestBed } from '@angular/core/testing';
-import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
-} from '@angular/platform-browser-dynamic/testing';
-
-getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting()
-);
-
-expect.addSnapshotSerializer(HTMLCommentSerializer);
-expect.addSnapshotSerializer(AngularSnapshotSerializer);
+import 'jest-preset-angular';
 import './jestGlobalMocks';
